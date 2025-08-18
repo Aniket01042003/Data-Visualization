887aaf114a6c035f119899c6911dafc2cd9526e6
Aniket Kapse <117436061+Aniket01042003@users.noreply.github.com>
2025-08-18 19:33:28 +0530

    Update vite.config.js

--- DV frontend/DV Web/vite.config.js
+++ DV frontend/DV Web/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/Data-Visualization'
+  base: process.env.VITE_BASE_PATH || '/Data-Visualization/DV frontend/DV web'
 })
